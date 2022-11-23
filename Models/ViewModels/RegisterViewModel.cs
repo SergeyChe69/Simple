2f8b4a3b9d6f888478f472597eb59db177235469
SergeyChe46 <4ss89@mail.ru>
2022-11-23 18:49:38 +0300

    Add model for user registration.

---
+++ Models/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,24 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace DotBlog.Models.ViewModels
+{
+    /// <summary>Fields for Register</summary>
+    public class RegisterViewModel
+    {
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Required]
+        [Compare("Password", ErrorMessage = "Password aren't comparing!!!")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Repeat password")]
+        public string PasswordConfirm { get; set; }
+    }
+}
